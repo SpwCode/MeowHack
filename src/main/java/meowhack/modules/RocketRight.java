d6bbf32ba841a04e428592b6b214ab552a71fd3c
SpwCode <83662999+SpwCode@users.noreply.github.com>
2025-02-28 11:12:16 +0500

    RocketRight проверка

--- src/main/java/meowhack/modules/RocketRight.java
+++ src/main/java/meowhack/modules/RocketRight.java
@@ -9,11 +9,14 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.*;
 import net.minecraft.item.Items;
 import net.minecraft.item.MiningToolItem;
 import net.minecraft.item.SwordItem;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 
 public class RocketRight extends Module {
     private double ticksLeft;
@@ -37,7 +40,7 @@ public class RocketRight extends Module {
     @EventHandler
     private void onTick(TickEvent.Post event) {
         if (ticksLeft <= 0) {
-            if (mc.options.useKey.isPressed() && this.mc.currentScreen == null && !(this.mc.player.isOnGround()) && (this.mc.player.getMainHandStack().getItem() instanceof SwordItem || this.mc.player.getMainHandStack().getItem() instanceof MiningToolItem || this.mc.player.getMainHandStack().isEmpty())) {
+            if (mc.options.useKey.isPressed() && this.mc.currentScreen == null  && mc.player.isGliding() && !(isLookingAtContainer()) && (this.mc.player.getMainHandStack().getItem() instanceof SwordItem || this.mc.player.getMainHandStack().getItem() instanceof MiningToolItem || this.mc.player.getMainHandStack().isEmpty())) {
                 FindItemResult rocket = InvUtils.find(Items.FIREWORK_ROCKET);
                 if (rocket.slot() > 8) {
                     InvUtils.move().from(rocket.slot()).toHotbar(8);
@@ -49,7 +52,7 @@ public class RocketRight extends Module {
                     ticksLeft = FireworkDelay.get() * 20;
                 } else {
                     InvUtils.swap(itemResult.slot(), true);
-                    mc.getNetworkHandler().sendPacket(new CloseHandledScreenC2SPacket(0));
+                    //mc.getNetworkHandler().sendPacket(new CloseHandledScreenC2SPacket(0));
                     mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
                     InvUtils.swapBack();
                     ticksLeft = FireworkDelay.get() * 20;
@@ -57,5 +60,17 @@ public class RocketRight extends Module {
             }
         }
         ticksLeft--;
+    }
+
+    private boolean isLookingAtContainer() {
+        if (mc.crosshairTarget == null || !(mc.crosshairTarget instanceof BlockHitResult)) {
+            return false;
+        }
+
+        BlockPos pos = ((BlockHitResult) mc.crosshairTarget).getBlockPos();
+        BlockState blockState = mc.world.getBlockState(pos);
+
+        return blockState.getBlock() instanceof InventoryProvider || blockState.getBlock() instanceof ChestBlock ||
+            blockState.getBlock() instanceof BarrelBlock || blockState.getBlock() instanceof ShulkerBoxBlock;
     }
 }
